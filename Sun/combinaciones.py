480e18a786940d307610c02ced7bc383d3aa790f
Nicaragua <santiago.lizondo@gmail.com>
2020-05-28 18:58:59 -0300

    m

--- Sun/combinaciones.py
+++ Sun/combinaciones.py
@@ -2,21 +2,9 @@ from itertools import permutations
 from pattern.es import *
 from pattern.web import Wiktionary
 import funciones
-import pattern.text.es
-import itertools
 
 ingreso = input('ingrese poqitas letras pq sino se traba (pone paz please): ')
 print('estas son las palabras que se forman con esas letras:')
-
-
-letras=str(input('ingrese letras')).split(' ')
-print(letras)
-combinaciones=list(itertools.permutations(letras))
-for x in combinaciones:
-    palabra=''
-    for y in x:
-        palabra=palabra+y
-    print(palabra)
 
 
 for p in permutations(ingreso):  ##pruebo todas las combinaciones posibles
