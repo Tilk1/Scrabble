e000b940f389831bc094704e0ad20a6be39d3e56
AgusMNS <agus.mns@hotmail.com>
2020-07-21 20:42:18 -0300

    gh

--- pruebas/test.py
+++ pruebas/test.py
@@ -1,22 +1,6 @@
-import PySimpleGUI as sg
-
-sg.theme('DarkBrown1')
-
-layout = [  [sg.Text('Stopwatch', size=(20, 2), justification='center')],
-            [sg.Text(size=(10, 2), font=('Helvetica', 20), justification='center', key='-OUTPUT-')],
-            [sg.T(' ' * 5), sg.Button('Start/Stop', focus=True), sg.Quit()]]
-
-window = sg.Window('Stopwatch Timer', layout)
-
-timer_running, counter = True, 0
-
-while True:                                 # Event Loop
-    event, values = window.read(timeout=10) # Please try and use as high of a timeout value as you can
-    if event in (None, 'Quit'):             # if user closed the window using X or clicked Quit button
-        break
-    elif event == 'Start/Stop':
-        timer_running = not timer_running
-    if timer_running:
-        window['-OUTPUT-'].update('{:02d}:{:02d}.{:02d}'.format((counter // 100) // 60, (counter // 100) % 60, counter % 100))
-        counter += 1
-window.close()
+def hola(qui):
+    qui.append('hola')
+pepe=list()
+pepe.append('lola')
+hola(pepe[:])
+print(pepe)
