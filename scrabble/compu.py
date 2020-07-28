50288901a4170cb159d17565802cfd7733963268
Nicaragua <santiago.lizondo@gmail.com>
2020-07-28 16:01:34 -0300

    avanzando con la IA

--- scrabble/compu.py
+++ scrabble/compu.py
@@ -20,17 +20,36 @@ def turno_maquina(tableroIm, tableroFichas, letrasM, window, colores, bolsa):
 
     ## Obtiene la palabra que puede formar
     print('PUEDO FORMAR LA PALABRA:')
-    print(combinaciones.intenta_las_combinaciones_quitando_una_letra(string_letras_maquina))
+    formada = (combinaciones.intenta_las_combinaciones_quitando_una_letra(string_letras_maquina))
+    tamaño = len(formada)
+    print('la palabra formada es: ', formada, 'de tamaño: ', tamaño)
 
     ## se para en una posicion al azar de libres
-    elegida = (random.choice(list(tableroIm)))
+    pos_elegida = (random.choice(list(tableroIm)))
+    coord_x = pos_elegida[0]
+    print('cordenada X : ',coord_x)
+    coord_y = pos_elegida[1]
+    print('cordenada Y : ',coord_y)
 
-    # comprueba que la casilla no este ocupada
-    print(tableroFichas.keys())
-    if elegida in tableroFichas.keys():
-        print('la posicion:', elegida, 'esta ocupada')
-    else:
-        print('esta disponible')
+    # comprueba que las casillas no esten ocupadas osea que no este en tableroFichas.Keys
+    # Pero tambien debo verificar que existan esas posiciones en el tablero, para q no se vaya a la cuarta dimension
+    print('OCUPADAS POR EL MOMENTO: ', tableroFichas.keys())
+
+    todas_disponibles = True
+
+    for i in range(tamaño):
+        if ((coord_x , i + coord_y) not in tableroFichas.keys()) & ((coord_x , i + coord_y) in tableroIm):
+            print('esta pos esta disponible: ', coord_x,i + coord_y)
+        else:
+            print('esta pos NO esta disponible: ', coord_x, i + coord_y)
+            todas_disponibles = False
+            break
+    
+
+    # si estan disponibles entonces las dibujo y agrego al diccionario de ocupadas
+    if todas_disponibles == True:
+        for i in range(tamaño):
+            window[(coord_x, i + coord_y)].update(image_filename='A.png')
 
     return
 
