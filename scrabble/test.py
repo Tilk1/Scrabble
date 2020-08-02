de0e03e8be8257fdaac9cda9d873b91a41e786d4
AgusMNS <agus.mns@hotmail.com>
2020-08-02 16:44:41 -0300

    compuandaconbolsa

--- scrabble/test.py
+++ scrabble/test.py
@@ -1,33 +1,4 @@
-from pattern.text.es import verbs, tag, spelling, lexicon, parse
-from sys import platform as _platform
-import os
-
-def clasificar(cual):
-    if cual == "JJ":
-        return "adjetivos"
-    elif cual == "VB":
-        return "verbos"
-    else:
-        return 'sustantivos'
-
-def tipoPalabra(d):
-    palabra = d
-    analisis = parse(palabra, tags=True, chunks=False).split(' ')
-    tipo = clasificar(analisis)
-    #if len(palabra) == 1:      SIRVE PARA TESTEAR POR AHORA
-    #    return 'no_existe'
-    if(tipo == 'sustantivos'):
-        if not palabra.lower() in verbs:
-            if not palabra.lower() in spelling:
-                if (not(palabra.lower() in lexicon) and not(palabra.upper() in lexicon) and not(palabra.capitalize() in lexicon)):
-                    return 'no_existe'
-                else:
-                    return clasificar(tag(palabra, tokenize=True, encoding='utf-8')[0][1])            
-            else:
-                return clasificar(tag(palabra, tokenize=True, encoding='utf-8')[0][1])
-        else:
-            return clasificar(tag(palabra, tokenize=True, encoding='utf-8')[0][1])
-    else:
-        return tipo
-
-print(tipoPalabra('correr'))
+import random
+turno=['compu','usuario']
+turno = random.choice(turno)
+print(turno)
