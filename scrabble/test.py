67f1a4da15db7b5c7197ae12002e90a5d294a855
AgusMNS <agus.mns@hotmail.com>
2020-08-05 15:11:09 -0300

    sfds

--- scrabble/test.py
+++ scrabble/test.py
@@ -1,6 +1,7 @@
-def bla(x):
-    return x+20
-
-x = bla 
-r=x(2)
-print(r)
+import json
+with open('posponer.txt','a+') as archivo: #lo abro en modo append ya que en caso de no existir el archivo, crea uno nuevo y no da error, y si existe, puedo agregar nuevos jugadores. 
+	if(archivo.tell()>0):   #Me fijo si el archivo esta vacio, si ya se registraron usuarios entonces tengo que agregar al diccionario ya creado
+		archivo.seek(0)     #voy al inicio del archivo para poder tomar los datos
+		jugadores=json.load(archivo)
+	else:
+		jugadores=dict()      #Si todavia no hubo jugadores entonces creo el diccionario
