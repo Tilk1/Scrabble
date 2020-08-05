3a2d3b31a03de8e258bcfdfca1eaea54596df0c1
AgusMNS <agus.mns@hotmail.com>
2020-08-05 12:22:24 -0300

    yest

--- scrabble/test.py
+++ scrabble/test.py
@@ -1,11 +1,20 @@
-pal='perro'
+
 letrasM = {'m0': 'P.png', 'm1': 'E.png', 'm2': 'R.png','m3': 'V.png', 'm4': 'R.png', 'm5': 'O.png', 'm6': 'L.png'}
 letrasM=list(letrasM.items())
-print(str.find('rr'))
+#print(pal.find('rr'))
 print(letrasM)
 #if(letrasM.count('RR.png')>=1):
-for x in letrasM:
-    pal.find(x[1].split('.')[0])
-    if(x[1]=='RR.png'):
-
-
+#for x in letrasM:
+#    if()
+#    if(pal.find(x[1].split('.')[0])!=-1):
+#
+#    if(x[1]=='RR.png'):(
+palabra=list()
+pal='perrorro'
+for x in (range(pal.count('rr'))):
+    #palabra.append('RR')
+    letrasM.
+pal=pal.replace('rr','')
+print(palabra)
+pal=','.join(pal).split(',')
+print(pal)
