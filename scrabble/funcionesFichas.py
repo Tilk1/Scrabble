33abb1157429223a12a55f6c2a35d3bcda872287
AgusMNS <agus.mns@hotmail.com>
2020-08-06 22:30:34 -0300

    comentariosfichas

--- scrabble/funcionesFichas.py
+++ scrabble/funcionesFichas.py
@@ -5,74 +5,99 @@ import os
 cwd = os.getcwd()
 
 def randomLetra(bolsa):   #elige una letra random de la bolsa y la quita d ela bolsa
+	"""
+    Elige una letra de forma aleatoria y le descuenta en su cant 1. Cuando la cantidad de una letra llega a 0 se eleimina de la bolsa
+	Cuando la bolsa se vacia, se retorna vacio para indicar la finalizacion del juego.
+
+    """
 	if(bolsa=={}):
 		return 'vacio'
 	else:
 		otro = True
 		while(otro):
-			letra = random.choice(list(bolsa))
-			if(bolsa[letra]['cant'] > 0):
+			letra = random.choice(list(bolsa))   #elijo aleatoriamente una letra
+			if(bolsa[letra]['cant'] > 0):   #me fijo si hay suficientes de esa letra para usar en la bolsa
 				otro = False
-				bolsa[letra]['cant'] = bolsa[letra]['cant']-1
+				bolsa[letra]['cant'] = bolsa[letra]['cant']-1   #descuento 1 a la cant de letras ya que la saco de la bolsa
 				if(bolsa[letra]['cant'] == 0):
 					del(bolsa[letra])
 		return letra
 
 def repartir(letras, bolsa, window):   #reparte las fichas al principio del juego y de cada jugada si faltan fichas
+	"""
+    Reparto fichas al usuario y a la maquina. Si esta vacia la bolsa directamente devuelvo vacio. En caso de contener letras
+	Repongo una letra en cada lugar del atril que no tenga una letra, cuando no tiene una letra en el atril va a estar guardado como
+	letra='', entonces va a recorrer el atril y usando la funcion randomLetra voy a reponer las que faltan.
+	Al inicio del juego se van a reponer las 7.
+	Retorno sigo cuando la bolsa sigue con letras, no finaliza el juego.
+
+    """
 	if(bolsa=={}):
 		return 'vacio'
 	else:
 		x=0
 		letra=''
-		q=list(letras.keys())[0].split('0')[0]
+		q=list(letras.keys())[0].split('0')[0]   #me dice si es usuario o maquina
 		while(letra!='vacio' and x<=6):
 			if(letras[q+str(x)]==''):    #Tengo en cuenta letras[x]=='' ya que al principio del juego, el diccionario letras es letrasU={'u0':'', 'u1':'','u2':'','u3':'','u4':'','u5':'','u6':''} y cuando no hay ninguna letra en el atril en esa pos
 				letra=randomLetra(bolsa)
 				if(letra!='vacio'):
-					print(q+str(x))
-					print(letra)
 					if(q=='u'):  #corroboro que no es la maquina, ya que las fichas de la maquina tienen que estar boca abajo y no mostar la letra, entonces en la interfaz no se actualizaria la imagen
 						window[q+str(x)].update(image_filename=os.path.join(cwd,'imagenes',letra))
 					letras[q+str(x)]=letra
 			x=x+1
-		print(letra)
-		print(bolsa)
-		print('  ')
-		if(letra!='vacio'):
+		if(letra!='vacio'):  #si mientras repartia me devolvió que la bolsa esta vacia, entonces va a devolver vacio y termina el juego
 			return 'sigo'
+		else:
+			return 'vacio'
 
 def sumarFicha(bolsa, copia, letra):
+	"""
+    Cuando hago un intercambio devuelvo las fichas a la bolsa. Cuando se devuelve a la bolsa una 
+	letra que ya no existe(se eliminó previamnete debido a que cant llegó a 0) devo volver a crearla en el diccionario con
+	su respectiva key. Este es el primer caso del if (bolsa.get(letra,0)==0).
+	El segundo caso es que se devuelva a la bolsa una letra qu sigue existiendo en esta, por lo que simplemente se accede a
+	ella y se suma. Este es el caso del else.
+
+    """
 	print('devuelvo a la bolsa: ',letra)
 	if(bolsa.get(letra,0)==0):
-		bolsa[letra]=copia[letra]
+		bolsa[letra]=copia[letra]   #creo la letra con su valor y cant en 1 
 		bolsa[letra]['cant']=1
 	else:
-		bolsa[letra]['cant']=bolsa[letra]['cant']+1
+		bolsa[letra]['cant']=bolsa[letra]['cant']+1  #sumo 1 a la bolsa de esa letra
 
 def intercambiarFichas(letras, bolsa, copia, window, cant):
+	"""
+    Intercambiar ficha. Cuando se intercambian las 7 fichas se entra a if(cant==7), no se pide al usuario que elija cuales quiere cambiar ya que
+	son todas. Cuando la cantidad es menor a 7, el usuario elige cuales letras quiere intercambiar.
+	La máquina usa también la función cuando no puede formar palabras después de repetidos intentos, entonces cambia todas las fichas
+	del atril.
+
+    """
 	if(bolsa=={}):
 		sg.popup('No quedan mas fichas en la bolsa, no se ha podido realizar el intercambio')
 	else:
 		letra=''
 		x=0
-		q=list(letras.keys())[0].split('0')[0]
+		q=list(letras.keys())[0].split('0')[0]  #se fija si es la maquina o el usuario
 		if(cant==7):                                          #cuando quiero intercambiar todas se hace automaticamentr y no elijo cuales de a una
 			while(letra!='vacio' and x<=6):
 				letra=randomLetra(bolsa)
 				if(letra!='vacio'):
-					print('intercambio la ficha',letra, 'de ',q+str(x))
-					if(q=='u'):
-						window[q+str(x)].update(image_filename=os.path.join(cwd,'imagenes',letra))
+					print('intercambio la ficha',letra, 'de ',q+str(x))  
+					if(q=='u'): 
+						window[q+str(x)].update(image_filename=os.path.join(cwd,'imagenes',letra))  #actualiza solamente el atril del usuario, las fichas de de la maquina estan dadas vueltas
 					sumarFicha(bolsa, copia, letras[q+str(x)])  #agrego la letra que intercambie a la bolsa
 					letras[q+str(x)]=letra
-					x=x+1
+					x=x+1  #voy al siguiente indice del atril
 		else:
 			cambios=list()    #no me deja intercambiar una letra que ya intercambie en ese momento
 			while(x<cant and letra!='vacio'):
 				sigo=True
 				while(sigo):      #En el caso de que el evento no sea algunas de las fichas del atril para cambiarla, entonces sigue pidiendo que se elija y no hace nada
 					event,_=window.read()
-					if (not event in cambios) and (event in ('u0', 'u1','u2','u3','u4','u5','u6')):
+					if (not event in cambios) and (event in ('u0', 'u1','u2','u3','u4','u5','u6')):  #se queda esperando que el usuario elija que letras intercambiar
 						letra=randomLetra(bolsa)
 						cambios.append(event)
 						if(letra!='vacio'):
@@ -90,11 +115,21 @@ def intercambiarFichas(letras, bolsa, copia, window, cant):
 
 
 def ponerFicha(window,letra, puestas, event):
+	"""
+    Coloca la ficha en el tablero. Actualiza la imagen en el tablero de la letra que se coloca.
+
+    """
 	window[event].update(image_filename=os.path.join(cwd,'imagenes',letra))
 	puestas[event]=letra
 	return False
 
 def sacarFicha(tableroI, puestas, originales, letras, event, window):
+	"""
+    Función que saca las fichas, cuando el evento es igual a sacar, saco todas las fichas, letras[x] son las que tiene en el atril, 
+	las vuelvo a poner en el original. Puestas tiene las letras puestas en esa jugada en el tablero. Se eliminan todas, ya que se sacan.
+	En caso de sacar solo una ficha individual, de Puestas  solo se saca la ficha individual y se actualiza el tablero.
+
+    """
 	if(event=='sacar'):     #saco todas las fichas
 		for y in puestas:
 			for x in originales:
@@ -111,6 +146,21 @@ def sacarFicha(tableroI, puestas, originales, letras, event, window):
 		puestas=puestas.pop(event)          												#sacas la letra de las que pusiste en el tablero, ya que no esta mas
 
 def colocarFicha(inter,tableroI,tableroF,letras, window, colores,coordPlay, bolsa, puestas, palpos):
+	"""
+    Tiene en cuenta todo el proceso de colocar fichas del usuario. Se tiene en cuenta que al tocar un indice del atril, el uduario puede estar queriendo 
+	seleccionar una ficha para colocarla en el tablero o tiene una en mano y se arrepintió, por lo que quiere devolverla.
+	Cuando el evento es una coordenada del tablero, el usuario puede estar queriendo colocar una ficha, en ese caso se tiene 
+	que asegurar que no hay una ficha puesta de jugadas anteriores o de esta misma. O puede estar queriendo seleccionar una ficha
+	ya puesta en el tablero y sacarla.
+	También se tiene que tener en cuenta que pase la primer palabra por el incio.
+	Se calcula si la palabra es válida según configuración y el pattern, se calcula el puntaje y se retora.
+	A la hora de colocar las fichas solo se admite que sean horizontales de izquierda a derecha o verticales de arriba hacia abajo.
+	La primer ficha de la palabra puede colocarse en cualquier lado, la segunda ficha determina la dirección de la palabra, es decir, si 
+	es hacia la derecha(horizontal) o hacia abajo(vertical), y las siguientes letras solo van a poder colocarse teniendo en
+	cuenta esa dirección.
+	Cuando se presiona intercambiar, sale de la función y pierde el turno.
+
+    """
 	originales=letras.copy()  #Fichas del atril que tenia en el comienzo de la jugada
 	#puestas=dict()            #Fichas que voy poniendo en el tablero en esa jugada
 	poner=False				  #Poner va a ser True cuando tenga una letra en mano para poner en el tablero
